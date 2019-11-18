1b613963d207655be029ad836cf1f47477bb1f49
temirkayaeva <40575841+temirkayaeva@users.noreply.github.com>
2019-11-18 12:29:42 +0300

    Update plugin.R

--- plugin.R
+++ plugin.R
@@ -1,6 +1,6 @@
-library(MASS) # Generation of multidimensional normal distribution
+library(MASS) # Подключаем библиотеку MASS для генерации многомерного нормального распределения
 
-# Center of normal distribution
+# Восстановление центра нормального распределения
 estimate_mu <- function(objects) {
   rows <- dim(objects)[1]
   cols <- dim(objects)[2]
@@ -11,7 +11,7 @@ estimate_mu <- function(objects) {
   return(mu)
 }
 
-# Covariation matrix of normal distribution
+# Восстановление ковариационной матрицы нормальногораспределения
 estimate_cov_matrix <- function(objects, mu) {
   rows <- dim(objects)[1]
   cols <- dim(objects)[2]
@@ -22,11 +22,9 @@ estimate_cov_matrix <- function(objects, mu) {
   return(sigma)
 }
 
-# Get coefficients of plug-in
+# Получение коэффициентов подстановочного алгоритма
 get_coeffs <- function(mu1, sigma1, mu2, sigma2) {
   # Line equation: a*x1^2 + b*x1*x2 + c*x2 + d*x1 + e*x2 + f = 0
-  # Inverse matrices
-
   determ1 <-det(sigma1)
   determ2 <-det(sigma2)
   a <- sigma1[2,2]/determ1
@@ -49,10 +47,10 @@ get_coeffs <- function(mu1, sigma1, mu2, sigma2) {
   return(c("x^2" = A, "y^2" = B, "xy" = C, "x" = D, "y" = E, "1" = F))
 }
 
-# Count of objects in each class
+# Количество объектов в каждом классе
 objects_count <- 300 
 
-# Generation of test data
+# Генерируем тестовые данные
 Sigma1 <- matrix(c(10, 0, 0, 1), 2, 2)
 Sigma2 <- matrix(c(6, 0, 0, 6), 2, 2)
 Mu1 <- c(1, 0)
@@ -60,14 +58,14 @@ Mu2 <- c(15, 0)
 xy1 <- mvrnorm(n = objects_count, Mu1, Sigma1)
 xy2 <- mvrnorm(n = objects_count, Mu2, Sigma2)
 
-# Assembling 2 classes in one sample xl
+# Собираем два класса в одну выборку
 xl <- rbind(cbind(xy1, 1), cbind(xy2, 2))
 
-# Drawing the training sample
+# Рисуем обучающую выборку
 colors <- c("blue2", "green3")
 plot(xl[ , 1], xl[ , 2], pch = 21, bg = colors[xl[ ,3]], asp = 1, xlab = "x", ylab = "y")
 
-# Evaluation
+# Оценивание
 objects_first <- xl[xl[,3] == 1, 1:2]
 objects_second <- xl[xl[,3] == 2, 1:2]
 mu1 <- estimate_mu(objects_first)
@@ -76,7 +74,7 @@ sigma1 <- estimate_cov_matrix(objects_first, mu1)
 sigma2 <- estimate_cov_matrix(objects_second, mu2)
 coeffs <- get_coeffs(mu1, sigma1, mu2, sigma2)
 
-# Drawing of discriminant function
+# Рисуем дискриминантую функцию
 x <- y <- seq(-10, 20, len = 100)
-z <- outer(x, y, function(x, y) coeffs["x^2"]*x^2 + coeffs["y^2"]*y^2 + coeffs["x*y"]*x*y + coeffs["x"]*x + coeffs["y"]*y + coeffs["1"])
+z <- outer(x, y, function(x, y) coeffs["x^2"]*x^2 + coeffs["y^2"]*y^2 + coeffs["xy"]*x*y + coeffs["x"]*x + coeffs["y"]*y + coeffs["1"])
 contour(x, y, z, levels = 0, drawlabels = FALSE, lwd = 2.5, col = "red", add = TRUE)
