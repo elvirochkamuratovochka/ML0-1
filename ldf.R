8d5423392d58a68eee128fa3543f8069f6cdd113
temirkayaeva <40575841+temirkayaeva@users.noreply.github.com>
2019-12-22 17:39:22 +0300

    Update ldf.R

--- ldf.R
+++ ldf.R
@@ -53,34 +53,26 @@ objectsOfSecondClass <- xl[xl[,3] == 2, 1:2]
 mu1 <- estimateMu(objectsOfFirstClass)
 mu2 <- estimateMu(objectsOfSecondClass)
 sigma <- estimateFisherCovarianceMatrix(objectsOfFirstClass, objectsOfSecondClass, mu1, mu2)
-
-inverseSigma <- solve(sigma)
-alpha <- inverseSigma %*% t(mu1 - mu2)
-mu_st <- (mu1 + mu2) / 2
-beta <- mu_st %*% alpha
-
-abline(beta / alpha[2,1], -alpha[1,1]/alpha[2,1], col =
-         "red", lwd = 3)
 mu<-rbind(mu1,mu2)
 
 classifier <- function(xy,m,s,lambda,Py)
 {
   n <- dim(mu)[2]
   p <- rep(0,n)
-  for(i in 1:n)
-  {
-    mu <- matrix(c(m[i,1],m[i,2]),1,2)
-    det <- det(sigma)
-    invsigma <- solve(sigma)
-    
-    b <- invsigma %*% t(mu) 
-    D <- -2*b[1,1]
-    E <- -2*b[2,1]
-    
-    F <- c(mu %*% invsigma %*% t(mu)) 
+  for(i in 1:n) {
+   mu <- matrix(c(m[i,1],m[i,2]),1,2)
+   det <- det(sigma)
+   a <- sigma[2,2]/det
+   b <- -sigma[2,1]/det
+   c <- -sigma[1,2]/det
+   d <- sigma[1,1]/det
+
+   A <- -2*mu[1]*a-mu[2]*b-mu[2]*c #x
+   B <- -mu[1]*b-mu[2]*c-2*mu[2]*d #y
+   C <- a*mu[1]^2 + mu[1]*mu[2]*b+ mu[1]*mu[2]*c+ d*mu[2]^2
     
     func <- function(x, y) {
-      f<-x*D + y*E + F
+      f<-x*A + y*B + C
     }
     f<-func(xy[1],xy[2])
     p[i] <- log(lambda*Py) - f
@@ -95,4 +87,31 @@ classifier <- function(xy,m,s,lambda,Py)
   }
   return(class)
 }
+
+drawline <- function(mu1,mu2,sigma) {
+inverseSigma <- solve(sigma)
+alpha <- inverseSigma %*% t(mu1 - mu2)
+mu_st <- (mu1 + mu2) / 2
+beta <- mu_st %*% alpha
+
+abline(beta / alpha[2,1], -alpha[1,1]/alpha[2,1], col = "red", lwd = 3)
+
+}
+
+x <- -10
+while(x < 40)
+{
+  y <- -10
+  while(y < 40)
+  {
+    xy <- c(x,y)
+    c <- classifier(xy,mu,sigma,lambda=1,P=0.5)
+    points(xy[1],xy[2], col=c)
+    y <- y+0.5
+  }
+  x <- x+0.5
+}
+
+
+drawline(mu1,mu2,sigma)
 
