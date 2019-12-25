83f83cb6af4ed27144340a432a49426ca263a524
temirkayaeva <40575841+temirkayaeva@users.noreply.github.com>
2019-12-25 20:17:12 +0300

    Update and rename regression.R to sources/regression.R

--- sources/regression.R
+++ sources/regression.R
@@ -1,5 +1,5 @@
 
-## Нормализация обучающей выборки
+
 trainingSampleNormalization <- function(xl)
 {
  n <- dim(xl)[2] - 1
@@ -10,7 +10,7 @@ trainingSampleNormalization <- function(xl)
  return (xl)
 }
 
-## Добавление колонки для из -1 для w0
+
 trainingSamplePrepare <- function(xl)
 {
  l <- dim(xl)[1]
@@ -19,17 +19,17 @@ trainingSamplePrepare <- function(xl)
 length.out = l), xl[, n + 1])
 }
 
-## Логарифмическая функция потерь
+
 lossLog <- function(x)
 {
  return (log2(1 + exp(-x)))
 }
-## Сигмоидная функция
+
 sigmoidFunction <- function(z)
 {
  return (1 / (1 + exp(-z)))
 }
-## Стохастический градиент для логистической регрессии
+
 sg.LogRegression <- function(xl)
 {
  l <- dim(xl)[1]
@@ -81,9 +81,9 @@ break
 
  return (w)
 }
-# Кол-во объектов в каждом классе
+
 ObjectsCountOfEachClass <- 100
-## Моделируем обучающие данные
+
 library(MASS)
 Sigma1 <- matrix(c(2, 0, 0, 10), 2, 2)
 Sigma2 <- matrix(c(4, 1, 1, 2), 2, 2)
@@ -92,13 +92,13 @@ xy2 <- mvrnorm(n=ObjectsCountOfEachClass, c(10, -10),
 Sigma2)
 xl <- rbind(cbind(xy1, -1), cbind(xy2, +1))
 colors <- c(rgb(255/255, 255/255, 0/255), rgb(0/255, 200/255, 0/255))
-## Нормализация данных
+
 xlNorm <- trainingSampleNormalization(xl)
 xlNorm <- trainingSamplePrepare(xlNorm)
-## Отображение данных
+
 
 plot(xlNorm[, 1], xlNorm[, 2], pch = 21, bg = colors[xl[,3]+ 2], asp = 1)
-## Логистическая регрессия
+
 w <- sg.LogRegression(xlNorm)
 abline(a = w[3] / w[2], b = -w[1] / w[2], lwd = 3, col = "red")
 for (i in 1:dim(xlNorm)[1]) {
